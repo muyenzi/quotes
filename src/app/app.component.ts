e26295ae3830475b7b551a0e92a7a25de70154f2
angelicty <muyenziraissa@gmail.com>
2019-08-23 13:18:26 +0200

    added the proverb-info components info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,6 @@ import { Proverb } from './proverb';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent{
-//   proverbs:Proverb =[
-//     {nom:'Dont cry because it is over, smile because it happened.',auteur:'Dr Seus',soumission:'Esther'},
-//     {nom:'Be yourself; everyone else is already taken.',auteur:'Dr wilde',soumission:'Ella'},
-//     {nom:'No one can make you feel inferior without your consent.',auteur:'Eleanor Roosevelt',soumission:'Rolly'},
-//   ]
 
-// }
 
 }
--- src/app/proverb/proverb.component.ts
+++ src/app/proverb/proverb.component.ts
@@ -10,7 +10,7 @@ export class ProverbComponent implements OnInit {
   proverbs:Proverb[] =[
     {nom:'Dont cry because it is over, smile because it happened.',auteur:'Dr Seus',soumission:'Esther'},
     {nom:'Be yourself; everyone else is already taken.',auteur:'Dr wilde',soumission:'Ella'},
-    {nom:'No one can make you feel inferior without your consent.',auteur:'Eleanor Roosevelt',soumission:'Rolly'},
+    {nom:'No one can make you feel inferior without your consent.',auteur:'Eleanor Roosevelt',soumission:'Rolly'}
   ];
 
   constructor() { }
