2bd0b73625a0a18775e2f85e15ea5a46d6530f9c
angelicty <muyenziraissa@gmail.com>
2019-08-23 10:19:41 +0200

    created the class proverb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Proverb } from './proverb';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  proverbs:Proverb =[
+    {nom:'Dont cry because it is over, smile because it happened.',auteur:'Dr Seus',soumission:'Esther'},
+    {nom:'Be yourself; everyone else is already taken.',auteur:'Dr wilde',soumission:'Ella'},
+    {nom:'No one can make you feel inferior without your consent.',auteur:'Eleanor Roosevelt',soumission:'Rolly'},
+  ]
+
 }
+
---
+++ src/app/proverb.ts
@@ -0,0 +1,5 @@
+export class Proverb {
+    nom:string;
+    auteur:string;
+    soumission:string;
+}
