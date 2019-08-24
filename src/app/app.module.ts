057db09f580b053608315123355c951e74d34c6e
angelicty <muyenziraissa@gmail.com>
2019-08-24 16:36:14 +0200

    the quote form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProverbComponent } from './proverb/proverb.component';
 import { ProverbInfoComponent } from './proverb-info/proverb-info.component';
 import { DaysCountPipe } from './days-count.pipe';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProverbComponent,
     ProverbInfoComponent,
-    DaysCountPipe
+    DaysCountPipe,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
