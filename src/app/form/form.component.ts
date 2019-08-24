98ff52180e3ce5083611417cb529295bbc63ee84
angelicty <muyenziraissa@gmail.com>
2019-08-24 19:03:13 +0200

    the adding quotes

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Proverb } from '../proverb';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
+  newProverb = new Proverb ("","","",new Date());
   constructor() { }
 
   ngOnInit() {
